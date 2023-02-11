a343cb15626f7d08f81a857f7cc0f6af018360ad
KateFedkova <kate.fedkova@gmail.com>
2023-02-11 13:02:49 +0200

    signup and login added

--- notepad/database/__init__.py
+++ notepad/database/__init__.py
@@ -1,3 +1,3 @@
-from .db_model import create_db
+from .db_model import create_db, session
 from .user import Users
 from .posts import Posts
--- notepad/routes.py
+++ notepad/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
-
-from notepad import app
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import login_required
+from werkzeug.security import generate_password_hash
+from notepad import app, login_manager
+from notepad.database import session, Posts, Users
 
 
 @app.route("/index")
@@ -9,12 +11,50 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = session.query(Users).where(Users.username == username).first()
+        if user:
+            flash("This user already exists")
+            #return redirect(url_for("login"))
+
+        new_user = Users(username=username, password=generate_password_hash(password))
+        session.add(new_user)
+        session.commit()
+        session.close()
+        return redirect("login")
     return render_template("signup.html")
 
 
- #
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # if request.method == "POST":
+    #     username = request.form["username"]
+    #     password = request.form["password"]
+    #     user = session.query(Users).where(Users.username == username).first()
+    #     if user:
+    #         flash("This user already exists")
+    #         return redirect(url_for("login"))
+    #
+    #     new_user = Users(username=username, password=generate_password_hash(password))
+    #     session.add(new_user)
+    #     session.commit()
+    #     session.close()
+    #     return redirect("login")
+    return render_template("login.html")
+
+
+@app.route("/add_post")
+@login_required
+def add_post():
+    return render_template("add_post.html")
+
+
+
+
  # <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
  #            <div class="navbar-nav">
  #                <a class="nav-item nav-link" href="{{ url_for('group_get') }}">Show groups</a>
